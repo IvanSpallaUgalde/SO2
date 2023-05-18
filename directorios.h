b01a669b96c677a0e8f4b66de6400a10cfcc76ac
IvanSpallaUgalde <ivanuib332@gmail.com>
2023-05-18 16:36:47 +0200

    Entrega 2

--- directorios.h
+++ directorios.h
@@ -1,6 +1,9 @@
 #include "ficheros_basico.h"
 #include "ficheros.h"
+
+
 #define TAMNOMBRE 60 
+#define PROFUNDIDAD 32
 
 //Defines de errores
 #define ERROR_CAMINO_INCORRECTO -2
@@ -11,6 +14,8 @@
 #define ERROR_ENTRADA_YA_EXISTENTE -7
 #define ERROR_NO_SE_PUEDE_CREAR_ENTRADA_EN_UN_FICHERO -8
 
+#define TAMFILA 100
+#define TAMBUFFER (TAMFILA*1000) //suponemos un máx de 1000 entradas, aunque debería ser SB.totInodos
 
 struct entrada
 {
@@ -18,9 +23,23 @@ struct entrada
     unsigned int ninodo;
 };
 
+//Nivel 7
 int extraer_camino(const char *camino, char *inicial, char *final, char *tipo);
 int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsigned int *p_inodo,
 unsigned int *p_entrada, char reservar, unsigned char permisos);
 void mostrar_error_buscar_entrada(int error);
+
+//Nivel 8
+int mi_creat(const char *camino, unsigned char permisos):
 int mi_dir(const char *camino, char *buffer);
 int mi_chmod(const char *camino, unsigned char permisos);
+int mi_stat(const char *camino, struct STAT *p_stat);
+
+//Nivel 9
+int mi_write(const char *camino, const void *buf, unsigned int offset, unsigned int nbytes);
+int mi_read(const char *camino, void *buf, unsigned int offset, unsigned int nbytes);
+
+struct UltimaEntrada{
+    char camino [TAMNOMBRE*PROFUNDIDAD];
+    int p_inodo;
+};
